5b4923bbdbfe6d8192ea01e8bd2364ba41c4b1dc
Vladimir Todorov <vladimirtodorov@Vladimirs-MacBook-Pro.local>
2025-02-16 12:30:18 +0200

    project setup

---
+++ Everyday Calx/Controller/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Everyday Calx
+//
+//  Created by Vladimir Todorov on 15.02.25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+        self.view.backgroundColor = .systemBlue
+    }
+
+
+}
+
